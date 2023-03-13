896cea01a4d22ac78014880bd5e577759e13a8ab
EC2 Default User <ec2-user@ip-172-31-3-220.ap-northeast-1.compute.internal>
2023-03-13 05:27:36 +0000

    add admin index show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Tag.create([
-  { name: 'タグ1' },
-  { name: 'タグ2' },
-  { name: 'タグ3' },
-  { name: 'タグ4' },
-  { name: 'タグ5' }
-])
+Admin.create!(
+   email: 'admin@email.com' ,
+   password: '123456', 
+)

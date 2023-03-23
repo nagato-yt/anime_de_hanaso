7476b0f0b82ec00bb50b59a25e31618717197d32
EC2 Default User <ec2-user@ip-172-31-3-220.ap-northeast-1.compute.internal>
2023-03-23 08:55:00 +0000

    chenge post

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -9,11 +9,11 @@ class Public::PostsController < ApplicationController
     @q_posts= @q.result(distinct: true)
     
     if params[:tag_ids]
-      @posts = []
+      @q_posts = []
       params[:tag_ids].each do |key, value|
-        @posts += Tag.find_by(name: key).posts if value == "1"
+        @q_posts += Tag.find_by(name: key).posts if value == "1"
       end
-      @posts.uniq!
+      @q_posts.uniq!
     end
   end
 
